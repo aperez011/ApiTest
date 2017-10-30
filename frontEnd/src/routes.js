fae5126b561977499d7b6316e3feb4f731b160e1
angel011 <aperez@encorerepair.om>
2017-10-29 23:29:37 -0400

    first commit

---
+++ frontEnd/src/routes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+  <Route name="app" path="/" handler={require('./components/app')}>
+    <DefaultRoute handler={require('./components/homePage')} />
+    <Route name="SearcRNC" handler={require('./components/compani/searchCompany')} />
+    <NotFoundRoute handler={require('./components/notFoundPage')} />
+  </Route>
+);
+
+module.exports = routes;
